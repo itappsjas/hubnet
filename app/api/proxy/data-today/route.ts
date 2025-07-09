d62e0e3527048eecd91b976525820385227f0471
Budiyanto Angga <budiyantoagga2089@gmail.com>
2025-07-09 14:40:56 +0700

    New Update 9

--- app/api/proxy/data-today/route.ts
+++ app/api/proxy/data-today/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
 
     const data = await res.json()
     return NextResponse.json(data)
-  } catch (err) {
+  } catch (_) {
     return NextResponse.json({ error: 'Gagal fetch dari API lokal' }, { status: 500 })
   }
 }
