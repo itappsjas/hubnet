8b099328c17273af956926b957671f5ee4ffe832
Budiyanto Angga <budiyantoagga2089@gmail.com>
2025-07-09 14:35:24 +0700

    New Update 8

--- app/api/proxy/data-today/route.ts
+++ app/api/proxy/data-today/route.ts
@@ -1,5 +1,5 @@
 // app/api/proxy/data-today/route.ts
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
 export async function GET() {
   const username = 'EpostHubnet2025'
